887d3cf2c7c753016e1032a6e6083107b4d0d43b
Aliya Lewis <aliyalewis5@gmail.com>
2019-05-01 03:38:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 require "pry"
 def oxford_comma(array)
-  if array.length == 2
+  if array.length == 1
+    return array 
+    elsif array.length == 2
   return array.join(" and ")
 else array.length >= 3
   # binding.pry
