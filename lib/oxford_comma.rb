ecc17eb0d73f0cb00e3f76598f0f3fa66d5d329e
Aliya Lewis <aliyalewis5@gmail.com>
2019-05-01 03:34:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,8 @@ def oxford_comma(array)
   if array.length == 2
   return array.join(" and ")
 else array.length >= 3
-  array.insert(-2, "and ")
   binding.pry
+  array.insert(-2, "and ")
   end
   array.join(", ")
 end
